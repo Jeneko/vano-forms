e33ec49abb2ae8bedd213cde58ac62068e9a81a9
Jeneko <jenekomail@gmail.com>
2023-10-18 21:28:02 +0300

    init: first commit

---
+++ data.js
@@ -0,0 +1,17 @@
+// Динамические данные пользователя (вместо JSON - настоящий JS Object)
+const data = {
+  name: 'Jen',
+  age: 100,
+  status: 'admin',
+  // и так далее
+};
+
+const BASE_URL = 'https://pyrus.com/t'; // базовая ссылка
+
+// ID форм
+const formIds = [
+  { id: 'uf899543', title: 'Форма А' },
+  { id: 'uf899544', title: 'Форма B' },
+  { id: 'uf899545', title: 'Форма C' },
+  // и так далее
+];
